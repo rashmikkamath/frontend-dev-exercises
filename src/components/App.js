d3bc09379981c4e66d722a6561b41fb55ad55f78
Rashmi Kamath <rashmikkamath@gmail.com>
2018-01-30 13:45:52 -0500

    Bar chart rendering with faux data

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,17 @@ import '../css/App.css';
 import * as d3 from "d3";
 import Barchart from './Barchart.js';
 
+
+
 export default class App extends Component {
   constructor(props){
     super(props);
     this.state = {};
-    //this.createBarChart = this.createBarChart.bind(this);
   }
 
   componentWillMount() {
     var self=this;
+    //read the csv file and extract required data to pass to barchart component
     d3.csv("census.csv", function(error,data){
       if(error){
         self.setState({loadError:true});
@@ -28,7 +30,6 @@ export default class App extends Component {
 
   render() {
     //handle cases where data has error loading or taking time to load
-    
     if(this.state.loadError){
       return <div>Error loading data</div>
     }
@@ -37,20 +38,25 @@ export default class App extends Component {
         return <div>Loading</div>
     }
     //manipulate data to extract required info to send to Barchart component
-    var dataForChart = d3.nest()
+    /*var dataForChart = d3.nest()
         .key(function(d) { return d.education_level; })
         .key(function(d){return d.over_50k})
-        .entries(this.state.chartData);
-        
+        .entries(this.state.chartData);*/
 
+    //Design UI with faux data
+    var fauxData = [{category:"Bachelors", per_over_50k:78},
+                    {category:"Associates", per_over_50k:67},
+                    {category:"No degree", per_over_50k:34},
+                    {category:"High School", per_over_50k:44},
+                    {category:"Some College", per_over_50k:4}
+    ];
+    
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome</h1>
         </header>
-      
-        <Barchart data={dataForChart}/>
-      
+        <Barchart data={fauxData}/>
       </div>
     );
   }
--- src/components/Barchart.js
+++ src/components/Barchart.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
+import * as d3 from "d3";
 
-export default class Barchart extends Component {
-	constructor(props){
-		super(props);
-		//this.setContext = this.setContext.bind(this);
-	}
+
+export default class Exp extends Component{
+	
 	componentDidMount(){
-		this.setContext();
-  	}
+		var chartData=this.props.data;
+		var formatPercent = d3.format(".0%");
 
-  	setContext(){
-  		//handle data to get x and y axis parameters
-  		var chartData = this.props.data;
-  		var yData = chartData.map(function(d,i){
-  			return d.key
-  		});
-  		
-  	}
+		//create svg with dimensions
+		var svg = d3.select("#chartholder")
+					.append("svg")
+					.attr("width",860)
+					.attr("height",300);
 
-  	render(){
+		var margin = {top: 20, right: 20, bottom: 30, left: 80},
+	    	width = svg.attr("width") - margin.left - margin.right,
+	    	height = svg.attr("height") - margin.top - margin.bottom;
 
-  		
-  		return(
-  			<div id ="chartholder"></div>
-  		)
+	    var g = svg.append("g")
+				   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	    
+	    //tooltip
+
+
+	    //define x & y axis range
+	    var x = d3.scaleLinear().range([0, width]);
+		var y = d3.scaleBand().range([height, 0]);
+
+		//prepare data to sort ascending order of percentage
+		chartData.sort(function(a, b) { return a.per_over_50k - b.per_over_50k; });
+		
+		// x and y domains 
+		x.domain([0, 1]);
+		y.domain(chartData.map(function(d) { return d.category; })).padding(0.1);
+
+		//draw the x axis  
+		g.append("g")
+        .attr("class", "x axis")
+       	.attr("transform", "translate(0," + height + ")")
+      	.call(d3.axisBottom(x).ticks(10).tickSizeInner([-height]).tickFormat(formatPercent).tickPadding(10));
+
+      	//draw the y axis
+      	 g.append("g")
+        .attr("class", "y axis")
+        .call(d3.axisLeft(y));
+        
+        //draw chart rectangles
+        g.selectAll(".bar")
+        .data(chartData)
+      	.enter().append("rect")
+        .attr("class", "bar")
+        .attr("background","red")
+        .attr("x", 0)
+        .attr("height", y.bandwidth())
+        .attr("y", function(d) { return y(d.category); })
+        .attr("width", function(d) { return x(d.per_over_50k)/100; })
+        .attr('fill', 'lightblue');
+
+		
+	}
+
+	render(){
+		return(
+			<div id="chartholder"></div>
+			)
 	}
 }
